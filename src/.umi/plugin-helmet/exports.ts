f1b2e903a8750d689b9bb922932d271cdfcaf6b6
taurusjun <taurusjun@hotmail.com>
2022-06-23 18:44:43 +0800

    fix: del umi

--- src/.umi/plugin-helmet/exports.ts
+++
@@ -1,3 +0,0 @@
-// @ts-nocheck
-// @ts-ignore
-export { Helmet } from '/Users/juwang1/PG/test/my/riskUI/node_modules/react-helmet';
--- src/.umi/plugin-layout/icons.ts
+++
@@ -1,8 +0,0 @@
-// @ts-nocheck
-
-  import SmileOutlined from '@ant-design/icons/es/icons/SmileOutlined';
-import TableOutlined from '@ant-design/icons/es/icons/TableOutlined'
-  export default {
-    SmileOutlined,
-TableOutlined
-  }
